fb9ed12698ad44c2cea1283e2cb865dbd9f8bb77
jakkyo <35409436+jakkyo@users.noreply.github.com>
2018-01-16 23:51:42 +0300

    Fix always receving string from storage

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@
 
 export default function canWriteStorageCheck(storage) {
   return Promise.resolve()
-  .then(() => storage.setItem('@@', 1))
+  .then(() => storage.setItem('@@', '1'))
   .then(() => storage.getItem('@@'))
-  .then((item) => item === 1 ? Promise.resolve() : Promise.reject())
+  .then((item) => item === '1' ? Promise.resolve() : Promise.reject())
   .then(() => storage.removeItem('@@'))
   .then(() => storage.getItem('@@'))
   .then((item) => item === null ? Promise.resolve() : Promise.reject())
   .then(() => true)
-  .catch(() => false);
+  .catch((error) => {
+    console.warn('canWriteStorageCheck. Can not write storage', error)
+    return false;
+  });
 }
